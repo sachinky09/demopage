bd10b83df7c0b27f7f7e92eb02995ebcdc584bba
sachinky09 <158481936+sachinky09@users.noreply.github.com>
2024-03-10 17:37:40 +0530

    AboutUs.jsx

--- src/Pages/Home/AboutUs.jsx
+++ src/Pages/Home/AboutUs.jsx
@@ -9,26 +9,25 @@ export default function AboutUs() {
           
           <h1 className="about-heading">Details</h1>
           <ul>
-            <li>SppedTech Showdown will be a full day event exclusively for freshers.</li>
+            <li>SpeedTech Showdown will be a full day event exclusively for freshers.</li>
             <li>First 50 teams will join the race </li>
             <li>Date: 11th March 2024</li>
             <br/>
-            <li>There will be 2 slots. Each slot will consist of 25 teams
+            <li>There will be 2 slots.
               <br/>
               <br/>
               <ul className="timings">
                 <li>Slot 1 in the morning</li>
                 <li>Slot 2 during the evening</li>
-                <li>Final will be played among top 10 teams at 8pm</li>
               </ul>
             </li>
             <br/>
             <li>Prizes:
               <ul className="prizes">
                 <br/>
-                <li>Winner team will be awarded with Cash Prize +TEDx Merchandise</li>
-                <li>Runner up team will be awarded with TEDx merchandise and a brand new rc car</li>
-                <li>Second Runner up team will be awarded with TEDx merchandise</li>
+                <li>Winner team will be awarded with Cash Prize + upcoming event Merchandise + brand new rc car</li>
+                <li>Runner up team will be awarded with upcoming event and a brand new rc car</li>
+                <li>Second Runner up team will be awarded with upcoming event merchandise</li>
               </ul>
             </li>
           </ul>
